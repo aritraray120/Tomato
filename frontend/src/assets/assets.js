7390b882916ffb037a18baee4cbe9b25360cf5d8
Aritra Ray <aritraray120@gmail.com>
2025-02-11 21:08:10 +0530

    Initial commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,31 @@
+import cart_icon from "./cart_icon.png";
+import cherry_image from "./cherry_img.png";
+import courier_contact_card from "./courier_contact_card.png";
+import food_arrived_img from "./food_arrived_img.png";
+import food_rate_card from "./food_rate_card.png";
+import foodie_women from "./foodie_women.png";
+import logo from "./logo.png";
+import mobile_order_snap from "./mobile_order_snap.png";
+import play_icon from "./play_icon.png";
+import search_icon from "./search_icon.png";
+import send_icon from "./send_icon.png";
+import take_away_img from "./take_away_img.png";
+import waiters_img from "./waiters_img.png";
+import women_ordering_img from "./women_ordering_img.png";
+
+export const assets = {
+    cart_icon,
+    cherry_image,
+    courier_contact_card,
+    food_arrived_img,
+    food_rate_card,
+    foodie_women,
+    logo,
+    mobile_order_snap,
+    play_icon,
+    search_icon,
+    send_icon,
+    take_away_img,
+    waiters_img,
+    women_ordering_img
+};
